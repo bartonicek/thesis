eb03de6e9228e057494058bc3b58c9cb153f305b
Adam Bartonicek <bartonicek@gmail.com>
2023-01-28 10:48:54 +1300

    Added "data, statistical summaries and graphics" section to litreview

--- _book/libs/plotscapeTest-binding-0.0.0.9000/plotscapeTest.js
+++ _book/libs/plotscapeTest-binding-0.0.0.9000/plotscapeTest.js
@@ -11,17 +11,19 @@ HTMLWidgets.widget({
 
       renderValue: function(x) {
 
+        console.log(x)
+
         // TODO: code to render the widget, e.g.
         el.classList.add("graphicDiv")
-        const scene = new PLOTSCAPE.Scene(el, new PLOTSCAPE.DataFrame(x.data))
+        const scene = new PLOTSCAPE.Scene(el, new PLOTSCAPE.DataFrame(x.data),
+                                          {layout : x.layout})
 
         if (!x.types) return
         const typeArray = Array.isArray(x.types) ? x.types : [x.types]
         typeArray.forEach((e, i) => {
           const mapping = x.mappings[i]
-          const dims = x.dims[i]
           const mappingArray = Object.keys(mapping).map(e => [e, mapping[e]])
-          scene.addPlotWrapper(e, new PLOTSCAPE.Mapping(...mappingArray), dims)
+          scene.addPlotWrapper(e, new PLOTSCAPE.Mapping(...mappingArray))
         })
 
       },
